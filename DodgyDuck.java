450d3dba634ee9e3683e7983ba72ddef0e183c75
ian <ianwentling@fungusnix.org>
2025-04-16 14:05:58 -0500

    test branch for game window

--- DodgyDuck.java
+++ DodgyDuck.java
@@ -1,2 +1,26 @@
-public class DodgyDuck {
+import javax.swing.*;
+import java.awt.*;
+
+public class DodgyDuck extends JPanel {
+
+
+    JPanel dodgyDuck = new JPanel();
+    int windowheight = 100;
+    int windowwidth = 100;
+
+    DodgyDuck() {
+
+
+        setPreferredSize(new Dimension(windowwidth,windowheight));
+        dodgyDuck.setLayout(null);
+        dodgyDuck.setLocation(0, 0);
+        setBackground(Color.blue);
+
+
+    }
+
+
+
+
+
 }
--- Main.java
+++ Main.java
@@ -1,15 +1,35 @@
+import javax.swing.JFrame;
 public class Main {
     public static void main(String[] args) {
 
-        //Creating Egg Objects
-        Egg myEgg = new Egg();
-        Egg bigEgg = new Egg(10,5,6,"Yellow");
+        //set game resolution
+        int gameheight = 1000;
+        int gamewidth = 1000;
 
-        //Accessing them
-        System.out.println("Big Egg's Size: " + bigEgg.getSize());
-        System.out.println("Big Egg's Speed: " + bigEgg.getSpeed());
-        System.out.println("Big Egg's Health: " + bigEgg.getHealth());
-        System.out.println("Big Egg's Color: " + bigEgg.getColor());
+//        //Creating Egg Objects
+//        Egg myEgg = new Egg();
+//        Egg bigEgg = new Egg(10,5,6,"Yellow");
+//
+//        //Accessing them
+//        System.out.println("Big Egg's Size: " + bigEgg.getSize());
+//        System.out.println("Big Egg's Speed: " + bigEgg.getSpeed());
+//        System.out.println("Big Egg's Health: " + bigEgg.getHealth());
+//        System.out.println("Big Egg's Color: " + bigEgg.getColor());
+
+
+        //create the game window
+        JFrame GameWindow = new JFrame();
+        GameWindow.setTitle("DodgyDuck");
+        GameWindow.setVisible(true);
+        GameWindow.setResizable(false);
+        GameWindow.setSize(gamewidth, gameheight);
+        DodgyDuck DodgyDuck = new DodgyDuck();
+        GameWindow.add(DodgyDuck);
+
+
+
+
+
     }
 
 }
